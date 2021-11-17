e5e7c03d673effb3e4d4f0d0d5c4e5a336e95324
exersalza <squidodle44@gmail.com>
2021-11-17 18:45:41 +0100

    Initial Push

---
+++ __main__.py
@@ -0,0 +1,48 @@
+import os
+import nextcord
+import platform
+from multiprocessing import Process
+
+from nextcord.ext import commands
+from cogs.etc.config import TOKEN, PREFIX
+from alive_progress import alive_bar
+from pyfiglet import Figlet
+
+
+intents = nextcord.Intents.all()
+bot = commands.Bot(command_prefix=PREFIX, intents=intents,
+                   help_command=None, description="Created by exersalza. Project: SunSide")
+
+
+count = 0
+
+for f in os.listdir('cogs'):
+    if f.endswith(".py") and f != "__init__.py" and f != "playground.py":
+      count += 1
+
+
+def load():
+    with alive_bar(count) as bar:
+        for filename in os.listdir("cogs"):
+            if filename.endswith(".py") and filename != "__init__.py" and filename != "playground.py":
+                bot.load_extension(f"cogs.{filename[:-3]}")
+                bar()
+
+
+if __name__ == '__main__':
+    platform = platform.system()
+
+    if platform == 'Windows':
+        clear = lambda: os.system('cls')
+        clear()
+    elif platform == 'Linux':
+        clear = lambda: os.system('clear')
+        clear()
+
+    print(f'\u001b[36m{Figlet(font="slant").renderText("")}\u001b[0m')
+    load()
+    Client = Process(target=bot.run(TOKEN))
+    Client.start()
+
+    
+    
