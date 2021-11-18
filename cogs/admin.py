d528aea296dda1aa17704fe7e45f45491a0fa0d8
exersalza <squidodle44@gmail.com>
2021-11-18 21:00:58 +0100

    Beginning Argparser

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,27 +1,51 @@
+from cogs.etc.config import dbBase, CUR, ESCAPE, DBESSENT
+from datetime import datetime
 from nextcord.ext import commands
 
 #todo:
 #   get, del, add, clear
+#       getUser
+#       getVehicleTrunk
+#       delUser
+#       delVehicles
+#       delWeapon
+#       clearWeapons
+#       clearInventory
+#       clearVehicleTrunk
+#       clearUserMoney
+#       addUserMoney
 
 
-class Admin(commands.cog):
+class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'Ready at {datetime.now().strftime("%H:%M:%S")}')
+
     @commands.command()
     async def get(self, ctx, *args):
+        CUR.execute(DBESSENT)
+
+        options = ['user', 'u',
+                   'vehtrunk', 'vt',
+                   'null']
+
+        if len(args):
+            for i in args:
+                print(i)
+
+    @commands.command()
+    async def delete(self, ctx, *args):
         pass
 
     @commands.command()
-    async def delete(self, ctx):
+    async def add(self, ctx, *args):
         pass
 
     @commands.command()
-    async def add(self, ctx):
-        pass
-
-    @commands.command()
-    async def clear(self, ctx):
+    async def clear(self, ctx, *args):
         pass
 
 
