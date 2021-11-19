de67e724df9d06148a32ca4e1c01d5b7743e6dd3
exersalza <>
2021-11-19 08:49:00 +0000

    Addded base for argparser

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,28 +1,56 @@
 from nextcord.ext import commands
+from datetime import datetime
+
+from etc.config import ESCAPE
 
 #todo:
 #   get, del, add, clear
+#       getUser
+#       getVehicleTrunk
+#       delUser
+#       delVehicles
+#       delWeapon
+#       clearWeapons
+#       clearInventory
+#       clearVehicleTrunk
+#       clearUserMoney
+#       addUserMoney
 
 
-class Admin(commands.cog):
-    def __init__(self, bot):
-        self.bot = bot
+class Admin(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
 
-    @commands.command()
-    async def get(self, ctx, *args):
-        pass
+	@commands.Cog.listener()
+	async def on_ready(self):
+		print(f'Ready at {datetime.now().strftime("%H:%M:%S")}')
 
-    @commands.command()
-    async def delete(self, ctx):
-        pass
 
-    @commands.command()
-    async def add(self, ctx):
-        pass
+	def parser(rounds, toparse, options=list) -> list:
+		return_list = []
+		
+		if ESCAPE + toparse in options:
+			for i in range(rounds):
+				pass
 
-    @commands.command()
-    async def clear(self, ctx):
-        pass
+	@commands.command()
+	async def get(self, ctx, *args):
+		options = [f'{ESCAPE}user', f'{ESCAPE}u',
+					f'{ESCAPE}vehicletrunk', f'{ESCAPE}vh']
+
+		
+
+	@commands.command()
+	async def delete(self, ctx):
+		pass
+
+	@commands.command()
+	async def add(self, ctx):
+		pass
+
+	@commands.command()
+	async def clear(self, ctx):
+		pass
 
 
 def setup(bot):
