e02709ef23440a20f91164872061c704e74a90a2
root <root@vmi497825.contaboserver.net>
2021-12-04 11:52:50 +0100

    changed startup

--- src/start.sh
+++ src/start.sh
@@ -1,5 +1,7 @@
-screen -X -S KenexarDcBot kill
+screen -X -S kenexardcbot kill
 
-screen -U -m -d -S KenexarDcBot python3.8 __main__.py
+screen -U -m -d -S kenexardcbot python3.8 __main__.py
 
 echo "Bot is started"
+
+eval "screen -r kenexardcbot"
