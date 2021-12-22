d4337d693ea7b35c0833bff4548d4384bbf25bbf
exersalza <squidodle44@gmail.com>
2021-12-22 13:34:39 +0100

    git updater finished, i hope :)

--- src/git-updater.py
+++ src/git-updater.py
@@ -1,4 +1,20 @@
-import sys
 import os
+import subprocess
+import time
+from datetime import datetime
 
-os.system('git status')
+
+def update_check(result=subprocess.run(['git', 'pull'], stdout=subprocess.PIPE)):
+    return result.stdout.decode('utf-8')
+
+
+if __name__ == '__main__':
+    while True:
+        if datetime.now().strftime('%H') in [12, 0]:
+            if 'already up to date.' in update_check().lower():
+                continue
+
+            time.sleep(120)
+            os.system('./restart.sh')
+
+        time.sleep(3600)
---
+++ src/restart.sh
@@ -0,0 +1,2 @@
+screen -X -S kenexardcbot kill
+screen -U -m -d -S kenexardcbot python3.8 __main__.py
