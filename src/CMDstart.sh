b0a0fd57eadb8cd9c49bf74b7798009743285008
exersalza <squidodle44@gmail.com>
2022-01-04 01:11:27 +0100

    Hope this works now

--- src/CMDstart.sh
+++ src/CMDstart.sh
@@ -1,5 +1,7 @@
 #! /home/MrPython/venv/bin/activate
 
+eval "source /home/MrPython/venv/bin/activate"
+
 screen -X -S kenexardcbot kill
 screen -U -m -d -S kenexardcbot python3.8 __main__.py
 
--- src/restart.sh
+++ src/restart.sh
@@ -1,4 +1,6 @@
 #! /home/MrPython/venv/bin/activate
 
+eval "source /home/MrPython/venv/bin/activate"
+
 screen -X -S kenexardcbot kill
 screen -U -m -d -S kenexardcbot python3.8 __main__.py
