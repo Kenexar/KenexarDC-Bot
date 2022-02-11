b021bb8e512c1c56185ca01d837e5bff5f3e7ad5
exersalza <squidodle44@gmail.com>
2022-02-11 11:30:30 +0000

    Added base API endpoints

--- API/main.py
+++ API/main.py
@@ -1,10 +1,29 @@
-from flask import Flask
-from flask_restful import Resource, Api, reqparse
-import pandas as pd
-import ast
+from uuid import uuid4
+
+from fastapi import FastAPI
+from models import User, Gender, Role
+from typing import Optional, List
+
+app = FastAPI(title='MrPythonAPI')
+
+db: List[User] = [
+    User(id=uuid4(), first_name='Jumilia', last_name='Ahmed', gender=Gender.female, roles=[Role.admin])
+
+]
 
 
-app = Flask(__name__)
-api = Api(app)
+@app.get('/')
+async def root():
+    return {'hello': 'mundo'}
 
+
+@app.get('/api/v1/users')
+async def fetch_users():
+    return db
+
+
+@app.post('/api/v1/users')
+async def add_user(user: User):
+    db.append(user)
+    return {'id': user.id}
 
---
+++ API/models.py
@@ -0,0 +1,23 @@
+from typing import Optional, List
+from uuid import uuid4, UUID
+from enum import Enum
+
+from pydantic import BaseModel
+
+
+class Role(str, Enum):
+    admin = 'admin'
+    mod = 'mod'
+
+
+class Gender(str, Enum):
+    male = 'male'
+    female = 'female'
+
+
+class User(BaseModel):
+    id: Optional[UUID] = uuid4()
+    first_name: str
+    last_name: str
+    gender: Gender
+    roles: List[Role]
