0962114db465967cea2929fdc93e5b55b8ce8f7f
exersalza <squidodle44@gmail.com>
2022-02-25 11:00:27 +0000

    Added Kenutils package

--- discord/kenutils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .faceit import *
-from .channel_listener import *
-from .checker import *
---
+++ discord/kenutils/src/core/__init__.py
@@ -0,0 +1,4 @@
+from .faceit import *
+from .channel_listener import *
+from .checker import *
+from .sqllite import *
